15347655b7b2d452364d32c7f92d09121f0d851d
Abdulkadirliban <abdulkadirliban0556@gmail.com>
2022-10-13 01:53:33 -0700

    i am rich

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: Text('I am Rich'),
+        backgroundColor: Colors.blueGrey[900],
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Image.asset('assets/images/diamond.png'),
+      ),
+    ),
+  ));
+}
